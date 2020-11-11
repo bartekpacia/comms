c43390da608ff867d16ac12c9cd630a3ac09cdec
Bartek Pacia <barpac02@gmail.com>
2020-11-12 00:19:12 +0100

    make uart_leds work nicely

--- uart_leds/uart_leds.go
+++ uart_leds/uart_leds.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/jacobsa/go-serial/serial"
 )
@@ -25,6 +24,18 @@ func init() {
 	flag.UintVar(&dataBits, "dbits", 8, "the number of data bits in a single frame")
 	flag.UintVar(&stopBits, "sbits", 1, "the number of stop bits in a single frame")
 	flag.IntVar(&parityMode, "pmode", 1, "parity mode, none = 0, odd = 1, even = 2")
+}
+
+func convert(b byte) byte {
+	shift := b - 48
+	var mask byte
+	mask = 0b00000001 << shift
+
+	if shift > 7 {
+		fmt.Printf("uart_leds: convert(%b): shift > 7, something is wrong\n", b)
+	}
+
+	return mask
 }
 
 func main() {
@@ -46,35 +57,38 @@ func main() {
 	defer port.Close()
 
 	fmt.Println("uart_leds: toggle LEDs on and off")
+	fmt.Println("uart_leds: enter 8 to exit")
 
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("enter a single byte: ")
-		text, err := reader.ReadString('\n')
+		b, err := reader.ReadByte()
 		if err != nil {
 			log.Fatalln("error reading from stdin:", err)
 		}
 
-		if strings.TrimSpace(text) == "STOP" {
+		if b == '\n' {
 			break
 		}
 
-		b := []byte(text)[0]
-		n, err := port.Write([]byte{b})
+		mask := convert(b)
+
+		n, err := port.Write([]byte{mask})
 		if err != nil {
 			log.Fatalln("error writing to serial port:", err)
 		}
-		fmt.Printf("wrote %d bytes to serial port (%d)\n", n, b)
 
-		output := make([]byte, 1)
-		n, err = port.Read(output)
-		if err != nil {
-			log.Fatalln("error reading from serial port:", err)
-		}
-		b = output[0]
-
-		fmt.Printf("read %d bytes from serial port (%d)\n", n, b)
+		fmt.Printf("wrote %d bytes to serial port (%d)\n", n, mask)
 	}
 
 	fmt.Println("finished :)")
 }
+
+// TODO: Move to a goroutine
+// output := make([]byte, 1)
+// n, err = port.Read(output)
+// if err != nil {
+// 	log.Fatalln("error reading from serial port:", err)
+// }
+// b = output[0]
+// fmt.Printf("read %d bytes from serial port (%d)\n", n, b)
